b7a8fa4c0e5702635fdd5b6d2e57ca4c8090a0ad
46534 <465343026@qq.com>
2021-10-21 11:15:45 +0800

    20211021

--- main.py
+++ main.py
@@ -297,6 +297,8 @@ class Application(Application_ui):
         #         print(aa[i])
         #    if aa[i].endswith('构配件'):
         #         print(aa[i])
+
+
         # TODO, Please finish the function here!
         # self.Text1.delete(0.0, END)
         # self.Label2Var = StringVar(value='管片')
